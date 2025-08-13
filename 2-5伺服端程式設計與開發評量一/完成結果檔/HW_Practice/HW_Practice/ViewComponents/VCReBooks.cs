e13dac3037f3e665800e19166183ecfada2ddf76
SuuYii <sue2gakupo@gmail.com>
2025-08-14 07:23:42 +0800

    feat(VCRebook): add ViewComponent to PostBooks_Index view feat(appsettings): separate environment-specific configurations

--- 2-5伺服端程式設計與開發評量一/完成結果檔/HW_Practice/HW_Practice/ViewComponents/VCReBooks.cs
+++ 2-5伺服端程式設計與開發評量一/完成結果檔/HW_Practice/HW_Practice/ViewComponents/VCReBooks.cs
@@ -1,5 +1,6 @@
 ﻿using GuestBooks.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace GuestBooks.ViewComponents
 {
@@ -12,12 +13,13 @@ namespace GuestBooks.ViewComponents
             _context = context;
         }
 
-        public async Task<IViewComponentResult> InvokeAsync(string bookID) 
-        { 
-        
-        
-        
-        
+        public async Task<IViewComponentResult> InvokeAsync(string bookID)
+        {
+            //取得回覆留言資料
+            var reBook = await _context.ReBook.Where(r => r.BookID== bookID).OrderByDescending(r => r.CreateDate).ToListAsync();
+
+            return View(reBook);
+
         }
 
 
